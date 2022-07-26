acea446604a5ccae98322a5b5532037a65e49a76
kage <96641989+zeesth@users.noreply.github.com>
2022-07-26 12:32:16 -0300

    Create coordinate converter.py

---
+++ coordinate converter.py
@@ -0,0 +1,17 @@
+# converts hours, minutes and seconds to decimal degrees
+
+def hms2dec(h, m, s):
+    dec = 15 * (h+m/60+s/(60*60))
+
+    return dec
+
+# converts degrees, minutes, and seconds to decimal degrees
+
+def dms2dec(d, m, s):
+    if d > 0:
+        dec = (d+m/60+s/(60*60))
+        
+    else:
+        dec = -1*((-1*d)+m/60+s/(60*60))
+
+    return dec
