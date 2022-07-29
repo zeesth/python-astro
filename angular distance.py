db24b3955459bb952e055b17c8aeccb95488c68b
kage <96641989+zeesth@users.noreply.github.com>
2022-07-29 17:56:53 -0300

    Update angular distance.py
    Added comments for clarity.

--- angular distance.py
+++ angular distance.py
@@ -1,10 +1,16 @@
 import numpy as np
 
+# calculates angular distance between two objects using right ascension and declination.
+
 def angular_dist(rasc1, dec1, rasc2, dec2):
+    # converts the distance to radians.
+    
     r1 = np.radians(rasc1)
     d1 = np.radians(dec1)
     r2 = np.radians(rasc2)
     d2 = np.radians(dec2)
+    
+    # get's the absolute distance between the objects and calculates the angle.
     
     deltar = np.abs(r1 - r2)
     deltad = np.abs(d1 - d2)
