255debab8a58f85302b43679755e53a767e1cd6e
kage <96641989+zeesth@users.noreply.github.com>
2022-07-29 17:59:09 -0300

    Update coordinate converter.py
    Added comments for clarity

--- coordinate converter.py
+++ coordinate converter.py
@@ -1,17 +1,19 @@
-# converts hours, minutes and seconds to decimal degrees
+# converts hours, minutes and seconds to decimal degrees.
 
 def hms2dec(h, m, s):
     dec = 15 * (h+m/60+s/(60*60))
 
     return dec
 
-# converts degrees, minutes, and seconds to decimal degrees
+# converts degrees, minutes, and seconds to decimal degrees.
 
 def dms2dec(d, m, s):
+    # analyse for negative values before conversion .
+    
     if d > 0:
         dec = (d+m/60+s/(60*60))
         
     else:
         dec = -1*((-1*d)+m/60+s/(60*60))
-
+        
     return dec
